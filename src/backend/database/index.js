bddb6a47869338994668cba81213a334372f0b55
Scott Henderson <scott.mc.henderson@gmail.com>
2021-09-26 11:35:34 -0700

    current database build

--- src/backend/database/index.js
+++ src/backend/database/index.js
@@ -1,2 +1,35 @@
 /* Routing for the Adulting OverFlow application */
+// TODO: checkLogin(login, password) {}
+// Login in page
+    // Check database information
+    // If matches, then procceed to main
+    // If false
 
+// Check username & database exist in the database
+//  If no username or password exist in database
+//      grab user input from fields, check against database
+//      error 401 -> "That account does not exist, enter a different acount name"
+//  If username & password don't match database records
+//      error 401 -> "Incorrect Username or Password"
+//  If so, do they match?
+//      If Yes to both, then get access to the website
+//      Redirect to main page
+
+// Registration Page
+// TODO: registration() {}
+//  When user hits submit, update information into the database records
+//      Check that username doesn't conflict with existing username
+//      If conflict exists, prevent database from updating
+//          Return error code 400, "User name already exists"
+//  If successfully updated, return message 201(?) that registration was successful
+//      Redirect to main page
+
+// Hash password function, return as object to update database
+
+// Check username and password exist in database
+//  Check bcrypt hash password matches one in the database
+//  Check that username matches one in database
+//      If true, then return true
+//      If not, then return false
+
+console.log('Hello World');
--- src/backend/database/passwordCheck.js
+++ src/backend/database/passwordCheck.js
@@ -1,13 +1,14 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const adminPassword = 'admin';
 const someOtherPlaintextPassword = 'not_bacon';
 
 // Test variables
 db = []; // Simulator database value
 
-bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-    console.log('Plain text: ', myPlaintextPassword);
+bcrypt.hash(adminPassword, saltRounds, function(err, hash) {
+    console.log('Plain text: ', adminPassword);
     console.log('Hash:', hash);
     db.push(hash);
     console.log(db);
