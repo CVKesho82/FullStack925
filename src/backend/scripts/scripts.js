22360de6951dc5ea577ba87f735f061fc7bf3e98
Scott Henderson <scott.mc.henderson@gmail.com>
2021-09-28 17:51:45 -0700

    database so far

--- src/backend/scripts/scripts.js
+++ src/backend/scripts/scripts.js
@@ -1,1 +1,23 @@
 /* Location for holding the scripts for various functions within the website */
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:')
+
+// Check password matches what is in the database
+function checkLogin() {
+    // Grab values from html form
+    let emailValue = document.getElementById("email").value;
+    let plaintextpassword = document.getElementById("password").value; 
+
+    // fetch 
+    fetch(login, {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: emailValue,
+          password: plaintextpassword,
+        }),
+      })
+}
