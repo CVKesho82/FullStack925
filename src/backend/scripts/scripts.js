f8635d5035f64500a8d4de4879eb79d694e40702
Scott Henderson <scott.mc.henderson@gmail.com>
2021-09-30 16:18:44 -0700

    updated files for login

--- src/backend/scripts/scripts.js
+++ src/backend/scripts/scripts.js
@@ -1,23 +1,34 @@
 /* Location for holding the scripts for various functions within the website */
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('sqlite::memory:')
 
 // Check password matches what is in the database
-function checkLogin() {
-    // Grab values from html form
-    let emailValue = document.getElementById("email").value;
-    let plaintextpassword = document.getElementById("password").value; 
+// const emailValue = document.getElementById("#input1").value;
+// const plaintextpassword = document.querySelector("#input2").value; 
 
-    // fetch 
-    fetch(login, {
+function checkLogin() {
+    // const userLogin = {email : emailValue, password : plaintextpassword  }
+    // console.log(userLogin);
+    // fetch from table
+    fetch('http://127.0.0.1:3000/login/verify', {
         method: "POST",
         headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: emailValue,
-          password: plaintextpassword,
-        }),
-      })
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(
+            {email: document.getElementById("input1").value,
+            password: document.getElementById("input2").value})
+    }).then (res => res.json())
+    .then (data => console.log(data))
+    .catch(function (err) {
+        console.log('something went wrong, call on database', err); // console.log the errors if any
+    });
+}
+
+// const loginForm = document.getElementsById("input1")
+
+
+function test() {
+    const email =  document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    console.log(email, password);
 }
