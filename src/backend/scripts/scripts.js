fbc928854177bd551f31bc0c85a7f9892e3ed617
Scott Henderson <scott.mc.henderson@gmail.com>
2021-09-30 19:04:15 -0700

    most recent login

--- src/backend/scripts/scripts.js
+++ src/backend/scripts/scripts.js
@@ -20,3 +20,24 @@ function checkLogin() {
         console.log('something went wrong, call on database', err); // console.log the errors if any
     });
 }
+
+function registration() {
+    console.log('DEBUG: Hit registration function');
+    fetch('http://127.0.0.1:3000/login/verify', {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(
+            {email: document.getElementById("registerEmail").value,
+            password: document.getElementById("registerPassword").value,
+            firstName: document.getElementById("registerFirst").value,
+            lastName: document.getElementsById("registerLast").value,
+        })
+    }).then (res => res.json())
+    .then (data => console.log(data))
+    .catch(function (err) {
+        console.log('something went wrong, register to database', err); // console.log the errors if any
+    });
+}
