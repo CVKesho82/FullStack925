d2d9468b83251a24b481e0e3d4b6ed4338ee5227
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2021-10-02 07:11:52 -0400

    Forum Question & Answer pages
    This folder contains: 
    -question page - draft layout
    -answer page - draft layout
    -working post and get (need to complete put and delete)
    -sequelize cli db model, seeder, configuration files
    -the node modules folder was removed
    
    -need to address why the submit question button stopped working

---
+++ frontend/forumPage/migrations/20211002094937-create-forum-questions.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('forumQuestions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      topic: {
+        type: Sequelize.STRING
+      },
+      question: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('forumQuestions');
+  }
+};
