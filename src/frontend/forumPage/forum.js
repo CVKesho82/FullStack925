523e60ed46ae6feb7122353ed9f356c39763040e
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2021-10-10 21:52:16 -0400

    WorkingDeletes&SubmitAnswerFunction

--- src/frontend/forumPage/forum.js
+++ src/frontend/forumPage/forum.js
@@ -30,7 +30,7 @@ const users = require("pg");
 //const pgp = require("pg-promise")();
 //const db = pgp("postgres://postgres:password@host:port/user");
 
-//---------------------QUESTIONS GET ROUTE (working get route) -------------------//
+//-------------------------QUESTIONS GET ROUTE (working get route) ----------------------//
 
 app.use(express.json());
 app.get('/forumQuestions', async (req, res) => {
@@ -48,7 +48,7 @@ app.post('/forumQuestions', async(req,res) => {
   });
 })
 
-//------------------------------QUESTION PUT ROUTE (working PUT)-------------------//
+//------------------------------QUESTION PUT ROUTE (working PUT)-----------------------//
 app.put('/forumQuestions/:id', async (req,res)=> {
   const {id} = req.params;
   const idFound = await forumQuestions.findByPk(id);
@@ -62,7 +62,7 @@ app.put('/forumQuestions/:id', async (req,res)=> {
   };
 });
 
-//------------------------SUBMIT Question function------------------------------>
+//-------------------------------SUBMIT Question function (working)------------------------------//
 function newQuestion(){
 fetch('http://127.0.0.1:4000/forumQuestions',{
   method:"POST",
@@ -78,7 +78,23 @@ fetch('http://127.0.0.1:4000/forumQuestions',{
   });
  }
 
-//------------------------ANSWERS GET ROUTE (working get route)-----------------------------//
+//----------------------------------QUESTION DELETE (working route)-------------------------//
+
+app.delete('/forumQuestions/:id', async (req,res)=> {
+  const {id} = req.params;
+  const oneQuestion = await forumQuestions.findByPk(id);
+  if (oneQuestion === null){
+    // console.log('Question id not found');
+    res.status(400).json({message:"That question id doesn't exist in database"});
+  }
+  else {
+    const deleteQuestion = await forumQuestions.destroy({where:{id}});
+    // console.log('deleted');
+    res.json(deleteQuestion);
+  };
+});
+
+//----------------------------ANSWERS GET ROUTE (working get route)--------------------------//
 app.use(express.json());
 app.get('/Answers', async (req, res) => {
   const answers = await Answers.findAll();
@@ -95,7 +111,25 @@ app.post('/Answers', async(req,res) => {
   });
 })
 
-//------------------------------ANSWER PUT ROUTE (working)---------------------------------/
+//---------------------------WORKING SUBMIT ANSWER FUNCTION (working)-----------------------//
+
+
+function submitAnswer(){
+  fetch('http://127.0.0.1:4000/Answers',{
+    method:"POST",
+    headers:{ 'Accept':'application/json','Content-Type': 'application/json'},
+    body:JSON.stringify(
+      {answer:document.getElementById('answer').value})
+  })
+    .then (res => res.json())
+    .then (data => console.log(data))
+    .catch(function (err) {
+        // console.log('wrong', err);
+    });
+   }
+
+
+//---------------------------------ANSWER PUT ROUTE (working)---------------------------------//
 app.put('/Answers/:id', async (req,res)=> {
   const {id} = req.params;
   const idFound = await Answers.findByPk(id);
@@ -109,6 +143,20 @@ app.put('/Answers/:id', async (req,res)=> {
   };
 });
 
+//----------------------------------ANSWER DELETE (working)------------------------------------//
+app.delete('/Answers/:id', async (req,res)=> {
+  const {id} = req.params;
+  const oneAnswer = await Answers.findByPk(id);
+  if (oneAnswer === null){
+    // console.log('Answer id not found');
+    res.status(400).json({message:"That answer id doesn't exist in database"});
+  }
+  else {
+    const deleteAnswer = await Answers.destroy({where: {id}});
+    // console.log('Answer deleted');
+    res.json(deleteAnswer);
+  };
+});
 
 
 
