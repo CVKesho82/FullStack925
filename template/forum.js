c55e4957cef87da1df418ea3d99ed5d3416ff8b7
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2021-10-14 15:01:25 -0400

    ConsoleLog/ElementbyID10_14_2021

--- template/forum.js
+++ template/forum.js
@@ -6,13 +6,13 @@ fetch('https://adultingfullstack.herokuapp.com/forumQuestions',{
   method:"POST",
   headers:{ 'Accept':'application/json','Content-Type': 'application/json'},
   body:JSON.stringify(
-    {topic:document.getElementById('topic').value,
+    {topic:document.getElementById('inputGroupSelect01').value,
     question:document.getElementById('question').value})
 })
   .then (res => res.json())
   .then (data => console.log(data))
   .catch(function (err) {
-      console.log('wrong', err); // console.log the errors if any
+//       console.log('wrong', err); // console.log the errors if any
   });
  }
 
@@ -28,7 +28,7 @@ function submitAnswer(){
       {answer:document.getElementById('answer').value})
   })
     .then (res => res.json())
-    .then (data => console.log(data))
+//     .then (data => console.log(data))
     .catch(function (err) {
         // console.log('wrong', err);
     });
